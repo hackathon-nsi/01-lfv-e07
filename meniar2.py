6b169ed9c0182904d1e861121173fe18d1992007
nsi-lfv-agm <79856419+nsi-lfv-agm@users.noreply.github.com>
2021-04-03 18:39:16 +0200

    Add files via upload

---
+++ meniar2.py
@@ -0,0 +1,33 @@
+
+from PIL import Image
+
+import urllib.request
+img= Image.open("images/photohackathon.jpg").convert("RGB")
+
+couleur = input (" choisissez la couleur de la photo, rouge, vert ou bleu")
+#print(img.size)
+width,height = img.size
+pixels= img.load()
+#print(pixels[1,2])
+
+for py in range(height):
+    for px in range(width):
+        r,g,b = img.getpixel((px,py))
+
+        if couleur == "rouge":
+            newr = r
+            newg = 0
+            newb = 0
+        if couleur =="bleu":
+            newr = 0
+            newg = 0
+            newb = b
+        if couleur =="vert":
+            newr = 0
+            newg = g
+            newb = 0
+
+
+        pixels[px,py] = (newr,newg,newb)
+img.show()
+
