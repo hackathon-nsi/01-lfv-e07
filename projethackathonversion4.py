bafcb0b6f5bb38879c1f3dfb4c109a82b4e58f42
Iris Dulat <79856060+nsi-lfv-dui@users.noreply.github.com>
2021-04-03 18:19:50 +0200

    Add files via upload

---
+++ projethackathonversion4.py
@@ -0,0 +1,50 @@
+from PIL import Image
+#from IPython.display import display
+import urllib.request
+
+# ouvrir une image hébergée sur internet
+##im = Image.open(urllib.request.urlopen('https://raw.githubusercontent.com/hackathon-nsi/h7n-nsi-01/main/images/washington.bmp'))
+im=Image.open('photohackathon.jpg')
+im.show()
+
+# créer une nouvelle image vide
+# le deuxième argument représente la taille de l'image et le troisième argument (optionnel) la couleur de remplissage au format RVB
+im_new = Image.new("RGB", (500, 500), (60, 0, 80))
+
+# informations sur l'image
+print(im.format, im.size, im.mode)
+
+# taille de l'image
+width, height = im.size
+
+endroit = input ("Où souhaitez-vous avoir la barre? (0-300)")
+endroit=int(endroit)
+
+
+
+
+
+
+# valeurs du pixel de coordonnées x, y (l'origine (0, 0) est en haut à gauche)
+for x in range(120):
+    for y in range(500):
+        pixel = im.getpixel((x+endroit, y))
+
+# valeurs des couleurs rouge, vert, bleu
+        p_rouge = pixel[0]
+        p_vert =  pixel[1]
+        p_bleu =  pixel[2]
+
+# modification du pixel de coordonnées x, y
+        im_new.putpixel((x+endroit,y),(p_rouge,p_vert,p_bleu))
+
+
+
+
+
+
+
+
+# affichage de l'image
+im_new.save('sortie.png')
+im_new.show()
