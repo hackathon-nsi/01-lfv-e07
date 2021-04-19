67e1cd9d8ff4785ea495004c29f6fdd7c843c282
Iris Dulat <79856060+nsi-lfv-dui@users.noreply.github.com>
2021-04-19 17:36:24 +0200

    Add files via upload

---
+++ meniar3.py
@@ -0,0 +1,48 @@
+
+from PIL import Image
+
+import urllib.request
+img= Image.open("photohackathon.jpg").convert("RGB")
+
+
+#print(img.size)
+width,height = img.size
+pixels= img.load()
+#print(pixels[1,2])
+
+def meniar():
+    couleur = input (" choisissez la couleur de la photo: rouge, jaune, violet, vert, cyan, blanc ou bleu ")
+    for py in range(height):
+        for px in range(width):
+            r,g,b = img.getpixel((px,py))
+
+            if couleur == "rouge":
+                newr = r
+                newg = 0
+                newb = 0
+            if couleur =="bleu":
+                newr = 0
+                newg = 0
+                newb = b
+            if couleur =="vert":
+                newr = 0
+                newg = g
+                newb = 0
+            if couleur == "jaune":
+                newr = r
+                newg = g
+                newb = 0
+            if couleur == "violet":
+                newr = r
+                newg = 0
+                newb = b
+            if couleur == "cyan":
+                newr = 0
+                newg = g
+                newb = b
+            if couleur == "blanc":
+                newr = r
+                newg = g
+                newb = b
+    pixels[px,py] = (newr,newg,newb)
+    img.show()
